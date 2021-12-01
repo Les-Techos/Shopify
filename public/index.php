fb7594f399b615532d063d0b17e212ea572b0ed0
Romain GENTY <romaingenty@outlook.com>
2021-12-01 14:47:57 +0100

    bootstrap added in index.php

--- public/index.php
+++ public/index.php
@@ -1,3 +1,18 @@
+<!Doctype HTML>
+<html>
+
+<head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1" />
+    <title>TP3 ISI WEB - Commande Pizza</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+</head>
+
+<body>
 <?php
 include_once('../Vue/Sign-in.php');
 ?>
+</body>
+
+</html>

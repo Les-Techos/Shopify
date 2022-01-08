7a115cad58a7a890e3e42127753b564ad0215293
Romain GENTY <romaingenty@outlook.com>
2022-01-08 09:52:15 +0100

    changement detail

--- controller/detailController.php
+++ controller/detailController.php
@@ -5,7 +5,7 @@ class detailController extends controller
 
     public function __construct()
     {
-        $this->controllerData  = "";
+        $this->controllerData["detailProduit"] = "";
     }
 
 
@@ -24,7 +24,7 @@ class detailController extends controller
     public function returnCards($Product_id)
     {
         $Product = new Product($Product_id);
-        $this->controllerData .= '
+        $this->controllerData["detailProduit"] .= '
         <div class="col-sm">
             <img src="../assets\image\\' . $Product->datas->image . '" style="max-width:100%;">
         </div>
@@ -45,74 +45,6 @@ class detailController extends controller
                 </div>
             </div>
         </div>
-        <div class="col-sm">
-            <div class="card" style="max-height: 600px; width: 500px;">
-            <h5 class="card-header"> Commentaire et note </h5> 
-                <div class="card-body" style="overflow-y: scroll">
-                
-                    Mettre une carte commentaire ici 
-                    Mettre une carte commentaire ici 
-                   
-                <div class="card mb-3" style="max-width: 480px;">
-                    <div class="row g-0">
-                        <div class="col-md-4">
-                            <img src="assets\image\femme.png" class="img-fluid rounded-start" alt="...">
-                        </div>
-                        <div class="col-md-8">
-                            <div class="card-body">
-                                <h5 class="card-title">Card title</h5>
-                                <p class="card-text">
-                                This is a wider card with supporting text below as a natural 
-                                lead-in to additional content. 
-                                This content is a little bit longer.
-                                </p>
-                                
-                                <p class="card-text"><small class="text-muted">Last updated 3 mins ago texte grise  la</small></p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                Mettre une carte commentaire ici 
-                Mettre une carte commentaire ici
-                Mettre une carte commentaire ici 
-                Mettre une carte commentaire ici
-                Mettre une carte commentaire ici 
-                Mettre une carte commentaire ici
-                Mettre une carte commentaire ici 
-                Mettre une carte commentaire ici
-                Mettre une carte commentaire ici 
-                Mettre une carte commentaire ici
-                Mettre une carte commentaire ici 
-                Mettre une carte commentaire ici
-                Mettre une carte commentaire ici 
-                Mettre une carte commentaire ici
-                Mettre une carte commentaire ici 
-                Mettre une carte commentaire ici
-                Mettre une carte commentaire ici 
-                Mettre une carte commentaire ici
-                Mettre une carte commentaire ici 
-                Mettre une carte commentaire ici
-                Mettre une carte commentaire ici 
-                Mettre une carte commentaire ici
-                Mettre une carte commentaire ici 
-                Mettre une carte commentaire ici
-                
-                </div> 
-                <div class="card-footer">
-                <div class="form-outline w-100">
-                <textarea
-                  class="form-control"
-                  id="textAreaExample"
-                  rows="1" style="background: #fff;"></textarea>
-               
-              </div>
-              <div class="float-end mt-2 pt-1">
-              <button type="button" class="btn btn-primary btn-sm">Post comment</button>
-              <button type="button" class="btn btn-outline-primary btn-sm">Cancel</button>
-            </div>
-                </div>   
-            </div>
-
-        </div>';
+       ';
     }
 }
